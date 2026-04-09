e9fea2edb19c6be966dfe996074a2c43f76c4f91
khalil <mac@MacBook-Pro-3.local>
2026-04-09 17:48:34 +0100

    ssss

--- ft_kaboom_1.py
+++ ft_kaboom_1.py
@@ -5,5 +5,4 @@ print("Test import now - THIS WILL RAISE AN UNCAUGHT EXCEPTION")
 
 from alchemy.grimoire.dark_spellbook import dark_spell_record
 
-# Le code n'arrivera jamais ici, le labo aura explosé avant.
 print(f"Testing dark spell: {dark_spell_record('Curse', 'bats, frogs')}")
